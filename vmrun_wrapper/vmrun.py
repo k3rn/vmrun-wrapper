a15e6c0847e65e9be3a6750d6417c3603d232f73
Mateus Kern <kern@mateuskern.com>
2014-11-26 15:10:42 -0200

    yup

--- vmrun_wrapper/vmrun.py
+++ vmrun_wrapper/vmrun.py
@@ -10,20 +10,15 @@ class vmrun():
             self.cli_path = ['/Applications/VMware Fusion.app/Contents'
                              '/Library/vmrun', '-T', 'fusion']
 
-    def __cli(self, arguments):
-        """
-        Execute the coomand.
-        Receives a list with the command, the path to the vm and additionals
-        parameters, in this exact order.
-        """
+    def _cli(self, arguments):
+        if arguments[0] != 'list' and not self.check_vmx_path(arguments[1]):
+            raise ValueError
+
         command = self.cli_path + arguments
-        print command
-        proc = subprocess.call(command)
+        proc = subprocess.Popen(command, stdout=subprocess.PIPE)
+        return proc.communicate()
 
     def check_vmx_path(self, vmx_path):
-        """
-
-        """
         if vmx_path[-1:] == '/':
             vmx_path = vmx_path[:-1]
 
@@ -37,9 +32,31 @@ class vmrun():
             return True
 
     def start(self, vmx_path, gui=False):
-        if self.check_vmx_path(vmx_path):
-            if not gui:
-                args = ['start', vmx_path, 'nogui']
-            else:
-                args = ['start', vmx_path, 'gui']
-            self.__cli(args)
+        if not gui:
+            self._cli(['start', vmx_path, 'nogui'])
+        else:
+            self._cli(['start', vmx_path, 'gui'])
+
+    def stop(self, vmx_path, hard=True):
+        if hard:
+            self._cli(['stop', vmx_path, 'hard'])
+        else:
+            self._cli(['stop', vmx_path, 'soft'])
+
+    def pause(self, vmx_path):
+        self._cli(['pause', vmx_path])
+
+    def unpause(self, vmx_path):
+        self._cli(['unpause', vmx_path])
+
+    def suspend(self, vmx_path, hard=True):
+        self._cli(['suspend', vmx_path])
+
+    def list(self):
+        result = self._cli(['list'])[0].split()
+        if int(result[3]) == 0:
+            return {'count': 0}
+        machines = list()
+        for n in range(4, len(result)):
+            machines.append(result[n])
+        return {'count': result[3], 'machines': machines}
