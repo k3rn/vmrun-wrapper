5c9d5a60491d56c1142297ea2d720694635fdf9b
Mateus Kern <kern@mateuskern.com>
2014-11-28 19:48:02 -0200

    Fix the tests

--- vmrun_wrapper/tests/vmrun.py
+++ vmrun_wrapper/tests/vmrun.py
@@ -7,14 +7,14 @@ class TestCheckers(unittest.TestCase):
     def setUp(self):
         self.vmrun = vmrun()
 
-    def test_remove_slash_check_vmx_path(self):
-        self.assertEqual(self.vmrun.check_vmx_path('vm.vmx/'), True)
+    def test_remove_slash_vmx_path_is_valid(self):
+        self.assertEqual(self.vmrun.vmx_path_is_valid('vm.vmx/'), True)
 
-    def test_with_vmwarevm_extension_check_vmx_path(self):
-        self.assertEqual(self.vmrun.check_vmx_path('vm.vmwarevm'), True)
+    def test_with_vmwarevm_extension_vmx_path_is_valid(self):
+        self.assertEqual(self.vmrun.vmx_path_is_valid('vm.vmwarevm'), True)
 
-    def test_invalid_extension_check_vmx_path(self):
-        self.assertEqual(self.vmrun.check_vmx_path('vmx.invalid'), False)
+    def test_invalid_extension_vmx_path_is_valid(self):
+        self.assertEqual(self.vmrun.vmx_path_is_valid('vmx.invalid'), False)
 
 if __name__ == "__main__":
     unittest.main()
